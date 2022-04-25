c994b13a0fac2e49d94e656a852cb880cc1ea2b5
Alexander Pavlov <alexppavlov93@gmail.com>
2022-04-25 19:26:31 +0300

    fix

--- ScraperWorkerService/ScrapingWorker.cs
+++ ScraperWorkerService/ScrapingWorker.cs
@@ -23,17 +23,21 @@ public class ScrapingWorker : BackgroundService
             .FollowLinks("a.topictitle")
             .Paginate("td>span.nav>a[href*='start=']")
             .WithScheme(new SchemaElement[] {
-                new("coverImageUrl", ".postImg", ContentType.Image),
+                new("coverImageUrl", ".postImg") { Type = ContentType.Image },
                 new("name", "div.postbody>span"),
                 new("categorization",
+                    new("category", "td:nth-child(2)>span>a:nth-child(2)"),
                     new("category", "td:nth-child(2)>span>a:nth-child(2)"),
                     new("subcategory", "td:nth-child(2)>span>a:nth-child(3)"),
                     new("genre",
                         new("category", "td:nth-child(2)>span>a:nth-child(2)"),
                         new("subcategory", "td:nth-child(2)>span>a:nth-child(3)"),
-                        new("subcategory", "td:nth-child(2)>span>a:nth-child(3)"))),
+                        new("subcategory", "td:nth-child(2)>span>a:nth-child(3)"),
+                        new("subcategory", "td:nth-child(2)>span>a:nth-child(3)")
+                    )
+                ),
                 new("torrentSize", "td.genmed>span"),
-                new("torrentLink", "a[href*='download.php?']", ContentType.Url)
+                new("torrentLink", "a[href*='download.php?']") { Type = ContentType.Url }
             })
             .WithParallelismDegree(10)
             .WriteTo.JsonFile("result.json")
--- WebReaper/Domain/SchemaElement.cs
+++ WebReaper/Domain/SchemaElement.cs
@@ -1,6 +1,5 @@
 namespace WebReaper.Domain;
 
-
 public record SchemaElement
 {
     public SchemaElement[]? Children { get; set; }
@@ -9,16 +8,16 @@ public record SchemaElement
 
     public string? Selector { get; set; }
 
-    public ContentType? Type { get; set; }
+    public SelectorType SelectorType { get; init; } = SelectorType.Css;
+
+    public ContentType? Type { get; init; } = ContentType.String;
 
     public SchemaElement(
         string field,
-        string selector,
-        ContentType type = ContentType.String)
+        string selector)
     {
         Field = field;
         Selector = selector;
-        Type = type;
     }
 
     public SchemaElement(
