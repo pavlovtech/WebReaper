5f9ada0e06b3038132e62da0a7261a4224855780
Alexander Pavlov <alexppavlov93@gmail.com>
2022-05-24 01:22:47 +0300

    update for logging

--- WebReaper.AzureFuncs/StartScrapting.cs
+++ WebReaper.AzureFuncs/StartScrapting.cs
@@ -37,16 +37,6 @@ namespace WebReaper.AzureFuncs
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
             try
 			{
 				var config = new ScraperConfigBuilder()
@@ -77,10 +67,14 @@ namespace WebReaper.AzureFuncs
 			}
 			catch (System.Exception ex)
 			{
-				return new OkObjectResult(ex.ToString());
+				_logger.LogError(ex, "Error occured");
+				return new OkObjectResult(ex);
 			}
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(new
+            {
+				Message = "good"
+            });
         }
     }
 }
