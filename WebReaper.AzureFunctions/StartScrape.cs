946000fb52d168003c4435e20fc2865e760202ab
Alexander Pavlov <alexppavlov93@gmail.com>
2022-05-23 16:28:26 +0300

    up

--- WebReaper.AzureFunctions/StartScrape.cs
+++ WebReaper.AzureFunctions/StartScrape.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,13 +6,13 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-// using WebReaper.Scraper;
-// using WebReaper.Domain.Parsing;
-// using WebReaper.Parsing;
-// using WebReaper.Queue.AzureServiceBus;
-// using System.Collections.Immutable;
-// using System.Linq;
-// using WebReaper.Domain;
+using WebReaper.Scraper;
+using WebReaper.Domain.Parsing;
+using WebReaper.Parsing;
+using WebReaper.Queue.AzureServiceBus;
+using System.Collections.Immutable;
+using System.Linq;
+using WebReaper.Domain;
 
 namespace WebReaper.AzureFunctions
 {
@@ -36,39 +35,39 @@ namespace WebReaper.AzureFunctions
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            // try
-            // {
-            //     var config = new ScraperConfigBuilder()
-            //         .WithLogger(log)
-            //         .WithStartUrl("https://rutracker.org/forum/index.php?c=33")
-            //         .FollowLinks("#cf-33 .forumlink>a")
-            //         .FollowLinks(".forumlink>a")
-            //         .FollowLinks("a.torTopic", ".pg")
-            //         .WithScheme(new Schema 
-            //         {
-            //             new("name", "#topic-title"),
-            //             new("category", "td.nav.t-breadcrumb-top.w100.pad_2>a:nth-child(3)"),
-            //             new("subcategory", "td.nav.t-breadcrumb-top.w100.pad_2>a:nth-child(5)"),
-            //             new("torrentSize", "div.attach_link.guest>ul>li:nth-child(2)"),
-            //             new Url("torrentLink", ".magnet-link"),
-            //             new Image("coverImageUrl", ".postImg")
-            //         })
-            //         .Build();
+            try
+            {
+                var config = new ScraperConfigBuilder()
+                    .WithLogger(log)
+                    .WithStartUrl("https://rutracker.org/forum/index.php?c=33")
+                    .FollowLinks("#cf-33 .forumlink>a")
+                    .FollowLinks(".forumlink>a")
+                    .FollowLinks("a.torTopic", ".pg")
+                    .WithScheme(new Schema
+                    {
+                         new("name", "#topic-title"),
+                         new("category", "td.nav.t-breadcrumb-top.w100.pad_2>a:nth-child(3)"),
+                         new("subcategory", "td.nav.t-breadcrumb-top.w100.pad_2>a:nth-child(5)"),
+                         new("torrentSize", "div.attach_link.guest>ul>li:nth-child(2)"),
+                         new Url("torrentLink", ".magnet-link"),
+                         new Image("coverImageUrl", ".postImg")
+                    })
+                    .Build();
 
-            //     var jobQueueWriter = new AzureJobQueueWriter("Endpoint=sb://webreaper.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=g0AAACe/NXS+/qWVad4KUnnw6iGECmUTJTpfFOMfjms=", "jobqueue");
+                var jobQueueWriter = new AzureJobQueueWriter("Endpoint=sb://webreaper.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=g0AAACe/NXS+/qWVad4KUnnw6iGECmUTJTpfFOMfjms=", "jobqueue");
 
-            //     await jobQueueWriter.WriteAsync(new Job(
-            //     config.ParsingScheme!,
-            //     config.BaseUrl,
-            //     config.StartUrl!,
-            //     ImmutableQueue.Create(config.LinkPathSelectors.ToArray()),
-            //     DepthLevel: 0));
-            // }
-            // catch (System.Exception ex)
-            // {
-                
-            //     return new OkObjectResult(ex.ToString());
-            // }
+                await jobQueueWriter.WriteAsync(new Job(
+                config.ParsingScheme!,
+                config.BaseUrl,
+                config.StartUrl!,
+                ImmutableQueue.Create(config.LinkPathSelectors.ToArray()),
+                DepthLevel: 0));
+            }
+            catch (System.Exception ex)
+            {
+
+                return new OkObjectResult(ex.ToString());
+            }
 
             return new OkObjectResult(responseMessage);
         }
