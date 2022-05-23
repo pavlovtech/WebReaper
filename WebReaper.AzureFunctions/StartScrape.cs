4107ffde1e5597023aff5d325cb6c74b2373a254
Alexander Pavlov <alexppavlov93@gmail.com>
2022-05-23 16:58:58 +0300

    depl fix

--- WebReaper.AzureFunctions/StartScrape.cs
+++ WebReaper.AzureFunctions/StartScrape.cs
@@ -29,7 +29,7 @@ namespace WebReaper.AzureFunctions
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name ??= data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
