209b0f9486e84c91b1d2edc5dd47053ffffa2a14
Alexander Pavlov <alexppavlov93@gmail.com>
2022-05-25 22:49:39 +0300

    optimization

--- WebReaper.Abstractions/LinkTracker/ICrawledLinkTracker.cs
+++ WebReaper.Abstractions/LinkTracker/ICrawledLinkTracker.cs
@@ -4,5 +4,7 @@ namespace WebReaper.LinkTracker.Abstract
     {
         Task AddVisitedLinkAsync(string siteUrl, string visitedLink);
         Task<IEnumerable<string>> GetVisitedLinksAsync(string siteName);
+        Task<IEnumerable<string>> GetNotVisitedLinks(string siteUrl, IEnumerable<string> links);
+        Task<long> GetVisitedLinksCount(string siteUrl);
     }
 }
