27b004874ec697ec219b8391f837bb10b5593e72
Alex Pavlov <alexppavlov93@gmail.com>
2022-10-10 12:56:29 +0300

    Console app example

---
+++ WebReaper.ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using WebReaper.Core;
+using WebReaper.Domain.Parsing;
+
+var webReaper = new Scraper()
+    .WithStartUrl("https://www.reddit.com/r/dotnet/")
+    .FollowLinks("a.SQnoC3ObvgnGjWt90zD9Z._2INHSNB8V5eaWp4P0rY_mE")
+    .Parse(new Schema
+    {
+        new("title", "._eYtD2XCVieq6emjKBH3m"),
+        new("text", "._3xX726aBn29LDbsDtzr_6E._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4")
+    })
+    .WriteToConsole()
+    .Run(10);
+
+Console.ReadKey();
