782c7f205adec37b7a4e4bfde45008e0b9c7962f
Alex Pavlov <alexppavlov93@gmail.com>
2022-10-11 23:12:36 +0300

    scheduler interface

---
+++ WebReaper/Core/IScheduler.cs
@@ -0,0 +1,11 @@
+﻿using WebReaper.Domain;
+
+namespace WebReaper.Core
+{
+    public interface IScheduler
+    {
+        void Enqueue(Job job);
+        void Enqueue(IEnumerable<Job> jobs);
+        Job Dequeue();
+    }
+}
