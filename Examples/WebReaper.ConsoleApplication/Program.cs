42855e1879ce566cdaa50dea46dcf9f11cc36527
Alex Pavlov <alexppavlov93@gmail.com>
2022-10-31 22:04:02 +0200

    fix

--- Examples/WebReaper.ConsoleApplication/Program.cs
+++ Examples/WebReaper.ConsoleApplication/Program.cs
@@ -1,5 +1,4 @@
-﻿using WebReaper.ConsoleApplication;
-using WebReaper.Core.Builders;
+﻿using WebReaper.Core.Builders;
 
 _ = new ScraperEngineBuilder("reddit")
     .GetWithBrowser("https://www.reddit.com/r/dotnet/")
@@ -10,7 +9,7 @@ _ = new ScraperEngineBuilder("reddit")
         new("text", "._3xX726aBn29LDbsDtzr_6E._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4")
     })
     .WriteToJsonFile("output.json")
-    .WithLogger(new ColorConsoleLogger())
+    .LogToConsole()
     .Build()
     .Run();
 
