0e48409130aee713e69771ca74956e42c0fa1ade
Alex Pavlov <alexppavlov93@gmail.com>
2022-11-09 00:31:19 +0200

    Refactoring

--- WebReaper/Scheduler/Concrete/InMemoryScheduler.cs
+++ WebReaper/Scheduler/Concrete/InMemoryScheduler.cs
@@ -8,20 +8,11 @@ public class InMemoryScheduler : IScheduler
 {
     private readonly Channel<Job> _jobChannel = Channel.CreateUnbounded<Job>();
 
-    public async ValueTask<Job> GetAsync(CancellationToken cancellationToken)
-    {
-        return await _jobChannel.Reader.ReadAsync(cancellationToken);
-    }
+    public async ValueTask<Job> GetAsync(CancellationToken cancellationToken) => await _jobChannel.Reader.ReadAsync(cancellationToken);
 
-    public IAsyncEnumerable<Job> GetAllAsync(CancellationToken cancellationToken = default)
-    {
-        return _jobChannel.Reader.ReadAllAsync(cancellationToken);
-    }
+    public IAsyncEnumerable<Job> GetAllAsync(CancellationToken cancellationToken = default) => _jobChannel.Reader.ReadAllAsync(cancellationToken);
 
-    public async ValueTask AddAsync(Job job, CancellationToken cancellationToken = default)
-    {
-        await _jobChannel.Writer.WriteAsync(job, cancellationToken);
-    }
+    public async ValueTask AddAsync(Job job, CancellationToken cancellationToken = default) => await _jobChannel.Writer.WriteAsync(job, cancellationToken);
 
     public async ValueTask AddAsync(IEnumerable<Job> jobs, CancellationToken cancellationToken = default)
     {
