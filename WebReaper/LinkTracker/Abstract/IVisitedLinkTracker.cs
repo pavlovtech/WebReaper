9e24f41a5a3b107e21f8a1378dc63fd477a74e00
Alex Pavlov <alexppavlov93@gmail.com>
2022-11-08 22:19:43 +0200

    refactoring

--- WebReaper/LinkTracker/Abstract/IVisitedLinkTracker.cs
+++ WebReaper/LinkTracker/Abstract/IVisitedLinkTracker.cs
@@ -3,8 +3,8 @@ namespace WebReaper.LinkTracker.Abstract
     public interface IVisitedLinkTracker
     {
         Task AddVisitedLinkAsync(string siteId, string visitedLink);
-        Task<IEnumerable<string>> GetVisitedLinksAsync(string siteId);
-        Task<IEnumerable<string>> GetNotVisitedLinks(string siteId, IEnumerable<string> links);
+        Task<List<string>> GetVisitedLinksAsync(string siteId);
+        Task<List<string>> GetNotVisitedLinks(string siteId, IEnumerable<string> links);
         Task<long> GetVisitedLinksCount(string siteId);
     }
 }
