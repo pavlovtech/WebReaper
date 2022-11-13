9002fd031e0720fb68a6cffd5ee0ca643803a904
Alex Pavlov <alexppavlov93@gmail.com>
2022-11-13 22:13:31 +0200

    renaming

--- WebReaper/Core/Builders/ScraperEngineBuilder.cs
+++ WebReaper/Core/Builders/ScraperEngineBuilder.cs
@@ -17,7 +17,7 @@ using WebReaper.PageActions;
 
 namespace WebReaper.Core.Builders;
 
-public class WebReaperBuilder
+public class ScraperEngineBuilder
 {
     private ConfigBuilder ConfigBuilder { get; } = new();
     private SpiderBuilder SpiderBuilder { get; } = new();
@@ -30,54 +30,54 @@ public class WebReaperBuilder
 
     protected IProxyProvider? ProxyProvider { get; set; }
 
-    public WebReaperBuilder(string siteId)
+    public ScraperEngineBuilder(string siteId)
     {
         SiteId = siteId;
     }
 
-    public WebReaperBuilder AddSink(IScraperSink sink)
+    public ScraperEngineBuilder AddSink(IScraperSink sink)
     {
         SpiderBuilder.AddSink(sink);
         return this;
     }
 
-    public WebReaperBuilder Authorize(Func<CookieContainer> authorize)
+    public ScraperEngineBuilder Authorize(Func<CookieContainer> authorize)
     {
         SpiderBuilder.Authorize(authorize);
         return this;
     }
 
-    public WebReaperBuilder IgnoreUrls(params string[] urls)
+    public ScraperEngineBuilder IgnoreUrls(params string[] urls)
     {
         SpiderBuilder.IgnoreUrls(urls);
         return this;
     }
 
-    public WebReaperBuilder Limit(int limit)
+    public ScraperEngineBuilder Limit(int limit)
     {
         SpiderBuilder.Limit(limit);
         return this;
     }
 
-    public WebReaperBuilder WithLinkTracker(IVisitedLinkTracker linkTracker)
+    public ScraperEngineBuilder WithLinkTracker(IVisitedLinkTracker linkTracker)
     {
         SpiderBuilder.WithLinkTracker(linkTracker);
         return this;
     }
     
-    public WebReaperBuilder TrackVisitedLinksInFile(string fileName)
+    public ScraperEngineBuilder TrackVisitedLinksInFile(string fileName)
     {
         SpiderBuilder.WithLinkTracker(new FileVisitedLinkedTracker(fileName));
         return this;
     }
     
-    public WebReaperBuilder TrackVisitedLinksInRedis(string connectionString)
+    public ScraperEngineBuilder TrackVisitedLinksInRedis(string connectionString)
     {
         SpiderBuilder.WithLinkTracker(new RedisVisitedLinkTracker(connectionString));
         return this;
     }
 
-    public WebReaperBuilder WithLogger(ILogger logger)
+    public ScraperEngineBuilder WithLogger(ILogger logger)
     {
         SpiderBuilder.WithLogger(logger);
 
@@ -86,26 +86,26 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder LogToConsole()
+    public ScraperEngineBuilder LogToConsole()
     {
         SpiderBuilder.WithLogger(new ColorConsoleLogger());
 
         return this;
     }
 
-    public WebReaperBuilder WriteToConsole()
+    public ScraperEngineBuilder WriteToConsole()
     {
         SpiderBuilder.WriteToConsole();
         return this;
     }
 
-    public WebReaperBuilder Subscribe(Action<JObject> scrapingResultHandler)
+    public ScraperEngineBuilder Subscribe(Action<JObject> scrapingResultHandler)
     {
         SpiderBuilder.AddSubscription(scrapingResultHandler);
         return this;
     }
 
-    public WebReaperBuilder WriteToCosmosDb(
+    public ScraperEngineBuilder WriteToCosmosDb(
         string endpointUrl,
         string authorizationKey,
         string databaseId,
@@ -115,43 +115,43 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder WriteToMongoDb(string connectionString, string databaseName, string collectionName)
+    public ScraperEngineBuilder WriteToMongoDb(string connectionString, string databaseName, string collectionName)
     {
         SpiderBuilder.AddSink(new MongoDbSink(connectionString, databaseName, collectionName, Logger));
         return this;
     }
 
-    public WebReaperBuilder WriteToCsvFile(string filePath)
+    public ScraperEngineBuilder WriteToCsvFile(string filePath)
     {
         SpiderBuilder.AddSink(new CsvFileSink(filePath));
         return this;
     }
 
-    public WebReaperBuilder WriteToJsonFile(string filePath)
+    public ScraperEngineBuilder WriteToJsonFile(string filePath)
     {
         SpiderBuilder.AddSink(new JsonLinesFileSink(filePath));
         return this;
     }
 
-    public WebReaperBuilder WithProxies(IProxyProvider proxyProvider)
+    public ScraperEngineBuilder WithProxies(IProxyProvider proxyProvider)
     {
         SpiderBuilder.WithProxies(proxyProvider);
         return this;
     }
 
-    public WebReaperBuilder Parse(Schema schema)
+    public ScraperEngineBuilder Parse(Schema schema)
     {
         ConfigBuilder.WithScheme(schema);
         return this;
     }
 
-    public WebReaperBuilder Get(string url)
+    public ScraperEngineBuilder Get(string url)
     {
         ConfigBuilder.Get(url);
         return this;
     }
 
-    public WebReaperBuilder GetWithBrowser(
+    public ScraperEngineBuilder GetWithBrowser(
         string url,
         Func<PageActionBuilder, ImmutableQueue<PageAction>>? actionBuilder = null)
     {
@@ -159,13 +159,13 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder Follow(string linkSelector)
+    public ScraperEngineBuilder Follow(string linkSelector)
     {
         ConfigBuilder.Follow(linkSelector);
         return this;
     }
 
-    public WebReaperBuilder FollowWithBrowser(
+    public ScraperEngineBuilder FollowWithBrowser(
         string linkSelector,
         Func<PageActionBuilder, ImmutableQueue<PageAction>>? actionBuilder = null)
     {
@@ -173,7 +173,7 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder Paginate(
+    public ScraperEngineBuilder Paginate(
         string linkSelector,
         string paginationSelector)
     {
@@ -181,7 +181,7 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder PaginateWithBrowser(
+    public ScraperEngineBuilder PaginateWithBrowser(
         string linkSelector,
         string paginationSelector,
         Func<PageActionBuilder, ImmutableQueue<PageAction>>? actionBuilder = null)
@@ -190,25 +190,25 @@ public class WebReaperBuilder
         return this;
     }
 
-    public WebReaperBuilder WithScheduler(IScheduler scheduler)
+    public ScraperEngineBuilder WithScheduler(IScheduler scheduler)
     {
         Scheduler = scheduler;
         return this;
     }
     
-    public WebReaperBuilder WithAzureServiceBusScheduler(string connectionString, string queueName)
+    public ScraperEngineBuilder WithAzureServiceBusScheduler(string connectionString, string queueName)
     {
         Scheduler = new AzureServiceBusScheduler(connectionString, queueName);
         return this;
     }
     
-    public WebReaperBuilder WithTextFileScheduler(string fileName, string currentJobPositionFileName)
+    public ScraperEngineBuilder WithTextFileScheduler(string fileName, string currentJobPositionFileName)
     {
         Scheduler = new FileScheduler(fileName, currentJobPositionFileName, Logger);
         return this;
     }
     
-    public WebReaperBuilder WithRedisScheduler(string connectionString, string queueName)
+    public ScraperEngineBuilder WithRedisScheduler(string connectionString, string queueName)
     {
         Scheduler = new RedisScheduler(connectionString, queueName, Logger);
         return this;
